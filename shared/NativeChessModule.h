dc804963872cd6ebf3986067c7cc4eca56c11873
kalersaab <gurwindersingh957@gmail.com>
2025-11-28 20:21:11 +0530

    chess engin using c++ rn

--- shared/NativeChessModule.h
+++ shared/NativeChessModule.h
@@ -1,24 +1,26 @@
-            #pragma once
+#pragma once
 
-            #include <ChessSpecsJSI.h> 
-            #include <memory>
-            #include <string>
-            #include <vector>
-            #include "ChessEngine.h"
+#include <ChessSpecsJSI.h>
+#include <memory>
+#include <string>
+#include <vector>
+#include "ChessEngine.h"
 
-            namespace facebook::react {
+namespace facebook::react
+{
 
-            class NativeChessModule : public NativeChessModuleCxxSpec<NativeChessModule> {
-            public:
-              NativeChessModule(std::shared_ptr<CallInvoker> jsInvoker);
+  class NativeChessModule : public NativeChessModuleCxxSpec<NativeChessModule>
+  {
+  public:
+    NativeChessModule(std::shared_ptr<CallInvoker> jsInvoker);
 
-              void reset(jsi::Runtime &rt);
-              bool makeMove(jsi::Runtime &rt, std::string move);
-              std::string getTurn(jsi::Runtime &rt);
-              jsi::Array getBoard(jsi::Runtime &rt);
+    void reset(jsi::Runtime &rt);
+    bool makeMove(jsi::Runtime &rt, std::string move);
+    std::string getTurn(jsi::Runtime &rt);
+    jsi::Array getBoard(jsi::Runtime &rt);
 
-            private:
-              std::unique_ptr<ChessEngine> engine;
-            };
+  private:
+    std::unique_ptr<ChessEngine> engine;
+  };
 
-            } // namespace facebook::react
+} // namespace facebook::react
